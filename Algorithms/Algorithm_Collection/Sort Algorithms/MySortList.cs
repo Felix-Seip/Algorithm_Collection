d4188fc0a86da6dbc2e47c4f0209c19d3c328a4f
Felix-Seip <felixseip01@gmail.com>
2016-12-21 13:53:48 +0100

    Initial Commit

---
+++ Algorithms/Algorithm_Collection/Sort Algorithms/MySortList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm_Collection.Sort_Algorithms
+{
+    public class MySortList<T> : List<T> where T : IComparable<T>
+    {
+        public void StupidSort()
+        {
+            int position = 0;
+            while (position < Count)
+            {
+                if (position == 0 || this[position].CompareTo(this[position - 1]) >= 0)
+                {
+                    position += 1;
+                }
+                else
+                {
+                    T temp = this[position - 1];
+                    this[position - 1] = this[position];
+                    this[position] = temp;
+                    position -= 1;
+                }
+            }
+        }
+
+        public bool IsSorted()
+        {
+            for(int i = 1; i < Count; i++)
+            {
+                if(this[i-1].CompareTo(this[i]) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Algorithms/Algorithm_Collection_Tests/SortTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm_Collection.Sort_Algorithms;
+
+namespace Algorithm_Collection_Tests
+{
+    [TestClass]
+    public class SortTests
+    {
+        [TestMethod]
+        public void StupidSortTest()
+        {
+            MySortList<int> mList = new MySortList<int>();
+            mList.Add(5);
+            mList.Add(1);
+            mList.Add(12);
+            mList.Add(40);
+            mList.Add(2);
+
+            mList.StupidSort();
+            Assert.IsTrue(mList.IsSorted());
+        }
+    }
+}
