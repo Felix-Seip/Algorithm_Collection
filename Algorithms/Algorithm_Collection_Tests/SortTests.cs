08c9e3338842dc94334697f2249bbabe9bb96982
ikke09 <development@fam-bechtluft.de>
2016-12-22 15:18:24 +0100

    Unit Tests angepasst

--- Algorithms/Algorithm_Collection_Tests/SortTests.cs
+++ Algorithms/Algorithm_Collection_Tests/SortTests.cs
@@ -7,18 +7,52 @@ namespace Algorithm_Collection_Tests
     [TestClass]
     public class SortTests
     {
+        private const int cListCount = 100;
+        private Random random;
+        MySortList<int> intList = new MySortList<int>();
+
+        [TestInitialize]
+        public void initTest()
+        {
+            random = new Random();
+            for(int i = 0; i < cListCount; i++)
+            {
+                intList.Add(random.Next(cListCount));
+            }
+        }
         [TestMethod]
         public void StupidSortTest()
         {
-            MySortList<int> mList = new MySortList<int>();
-            mList.Add(5);
-            mList.Add(1);
-            mList.Add(12);
-            mList.Add(40);
-            mList.Add(2);
+            intList.StupidSort();
+            Assert.IsTrue(intList.IsSorted());
+        }
 
-            mList.StupidSort();
-            Assert.IsTrue(mList.IsSorted());
+        [TestMethod]
+        public void InsertionSortTest()
+        {
+            intList.InsertionSort();
+            Assert.IsTrue(intList.IsSorted());
+        }
+
+        [TestMethod]
+        public void BubbleSortTest()
+        {
+            intList.BubbleSort();
+            Assert.IsTrue(intList.IsSorted());
+        }
+
+        [TestMethod]
+        public void SelectionSortTest()
+        {
+            intList.SelectionSort();
+            Assert.IsTrue(intList.IsSorted());
+        }
+
+        [TestMethod]
+        public void CombSortTest()
+        {
+            intList.CombSort();
+            Assert.IsTrue(intList.IsSorted());
         }
     }
 }
