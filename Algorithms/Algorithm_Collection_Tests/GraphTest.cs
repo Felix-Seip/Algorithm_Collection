419f139c5ce6410f832ae968a2d70141f7f22ebc
Christian Bechtluft <development@fam-bechtluft.de>
2017-03-19 17:01:15 +0100

    Unittest for Graphs
    + unfinished unittest for graph algorithms

---
+++ Algorithms/Algorithm_Collection_Tests/GraphTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Algorithm_Collection.Graph;
+
+namespace Algorithm_Collection_Tests
+{
+	[TestClass]
+	public class GraphTest
+	{
+		public List<Graph> _Graphs;
+
+		private const string cPathToTestData = @"TestData";
+		[ClassInitialize]
+		public void Init()
+		{
+			_Graphs = new List<Graph>();
+			_Graphs.Add(new Graph(cPathToTestData + "CityMap.graph"));
+			_Graphs.Add(new Graph(cPathToTestData + "Klausur.graph"));
+		}
+
+		[TestMethod]
+		public void DijkstraTest()
+		{
+			Route actual;
+
+			foreach(Graph g in _Graphs)
+			{
+				actual = Algorithm_Collection.GraphAlgorithm.Dijkstra.FindShortestPath(g);
+			}
+
+
+		}
+	}
+}
